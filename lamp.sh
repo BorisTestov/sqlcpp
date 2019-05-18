3e7647485444d092365146924957df22538780bb
Boris Testov <boris.testov@gmail.com>
2019-05-18 23:48:18 +0300

    Typos in script fixed

--- lamp.sh
+++ lamp.sh
@@ -13,14 +13,14 @@ sudo apt install mysql-server
 sudo mysql_secure_installation
 echo -e "${COLOR}Installing PHP${NC}"
 sudo apt install php libapache2-mod-php php-mysql
-sudo apt install sudo apt-get install php-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm,cli} -y
+sudo apt install php-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm,cli} -y
 sudo bash -c 'echo "<IfModule mod_dir.c>" > /etc/apache2/mods-enabled/dir.conf'
 sudo bash -c 'echo "    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf'
 sudo bash -c 'echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf'
 sudo a2enmod proxy_fcgi setenvif
 sudo a2enconf php7.2-fpm
 sudo systemctl restart apache2
-sudo bash -c 'echo "<?php" >> /var/www/html/info.php'
+sudo bash -c 'echo "<?php" > /var/www/html/info.php'
 sudo bash -c 'echo "phpinfo();" >> /var/www/html/info.php'
 sudo bash -c 'echo "?>" >> /var/www/html/info.php'
 sudo apt install libmysql++*
